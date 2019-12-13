64e2225047ba693403a77c5e182fb12c9246a61e
jinwei <jl5255@columbia.edu>
2019-12-13 12:05:08 -0500

    Updates

---
+++ precache-manifest.5244cf51d76d1a7303a0250ad9d5aec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a28db9657d81a95eed3492573fa95f",
+    "url": "/finalApp/index.html"
+  },
+  {
+    "revision": "ca0bab81b133a6dbd785",
+    "url": "/finalApp/static/css/main.681c075c.chunk.css"
+  },
+  {
+    "revision": "3cf4ccbe8f261dd31664",
+    "url": "/finalApp/static/js/2.b1622b61.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/finalApp/static/js/2.b1622b61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca0bab81b133a6dbd785",
+    "url": "/finalApp/static/js/main.ecb6dde0.chunk.js"
+  },
+  {
+    "revision": "0b50cf25c5d9fdf3660f",
+    "url": "/finalApp/static/js/runtime-main.0ab960c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/finalApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
